fb1679d301ee904e136622ee443edfeb8f5a1b4a
Geremia Taglialatela <tagliala.dev@gmail.com>
2016-01-13 18:43:00 +0100

    Support case insensitive confirmation validator Refers to #645

--- test/javascript/public/test/validators/confirmation.js
+++ test/javascript/public/test/validators/confirmation.js
@@ -3,10 +3,12 @@ module('Confirmation options', {
     $('#qunit-fixture')
       .append('<input id="password" type="password" />')
       .append('<input id="password_confirmation" type="password" />')
+      .append('<input id="username" type="text" />')
+      .append('<input id="username_confirmation" type="text" />')
   }
 });
 
-test('when values match', function() {
+test('when values match (case sensitive)', function() {
   var password_element = $('#password');
   var password_confirmation_element = $('#password_confirmation');
   var options = { message: "failed validation" };
@@ -22,4 +24,13 @@ test('when values do not match', function() {
   password_element.val('test');
   password_confirmation_element.val('bad test');
   equal(ClientSideValidations.validators.local.confirmation(password_element, options), "failed validation");
+});
+
+test('when values match (case insensitive)', function() {
+  var username_element = $('#username');
+  var username_confirmation_element = $('#username_confirmation');
+  var options = { message: "failed validation" };
+  username_element.val('tEsT');
+  username_confirmation_element.val('test');
+  equal(ClientSideValidations.validators.local.confirmation(username_element, options), undefined);
 });
